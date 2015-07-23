bad571cfda030ff1368b331c8180567340241bb2
Eugene Rodionov <git@theaqua.im>
2015-07-23 20:25:56 +0300

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# storage.js
+# dtStorage.js
+
+ES6 class for REST-like management local- & sessionStorage.
+
+## Features
+* ES6
+* local copy of storage (Safari [have bug with storage in private browsing mode](http://stackoverflow.com/questions/14555347/html5-localstorage-error-with-safari-quota-exceeded-err-dom-exception-22-an))
+
+## Install
+
+    npm install --save dtstorage.js
+
+## Usage
+#### constructor(provider)
+
+    import dtStorage from 'dtstorage';
+    const MyStorage = new dtStorage(provider)
+
+where `provider` is `localStorage` or `sessionStorage`.
+
+#### get(resource)
+get `resource` data
+
+#### post(resource, body)
+create new entry in resource
+
+#### patch(resource, key, value)
+set `key=value` in `resource`
+
+#### del(resource)
+delete `resource`
---
+++ src/storage.js
@@ -0,0 +1,63 @@
+import clone from 'clone';
+import utils from './utils';
+
+class dtStorage {
+  constructor(provider) {
+    this.provider = provider;
+    this.storage = clone(provider);
+  }
+
+  get(resource) {
+    const { provider, storage } = this;
+    const data = storage[resource];
+
+    if(data) {
+      return data;
+    } else {
+      throw new ReferenceError(404);
+    }
+  }
+
+  post(resource, body) {
+    const { provider, storage } = this;
+    storage[resource] = body;
+
+    try {
+      provider.setItem(resource, utils.JSONToString(body));
+
+      return storage[resource];
+    } catch(e) {
+      throw new Error(e);
+    }
+  }
+
+  patch(resource, key, value) {
+    const { provider, storage } = this;
+
+    storage[resource][key] = value;
+
+    try {
+      let data = utils.stringToJSON(provider.getItem(resource));
+      data[key] = value;
+
+      provider.setItem(resource, utils.JSONToString(data))
+    } catch(e) {
+      throw new Error(e);
+    }
+
+    return storage[resource];
+  }
+
+  del(resource) {
+    const { provider, storage } = this;
+    delete provider[resource];
+
+    try {
+      provider.removeItem(resource);
+    } catch(e) {
+      throw new Error(e);
+    }
+  }
+}
+
+export default dtStorage;
